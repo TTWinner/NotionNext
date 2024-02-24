a3505ed898c13cbac17d8e98fa1c57c1fb5d3d2d
TTWinner <118160230+TTWinner@users.noreply.github.com>
2024-02-24 22:24:25 +0800

    Update config.js

--- themes/landing/config.js
+++ themes/landing/config.js
@@ -3,8 +3,8 @@ const CONFIG = {
   //LANDING_HEADER_BUTTON_1_TITLE: 'Github开源',
   //LANDING_HEADER_BUTTON_1_URL: 'https://github.com/tangly1024/NotionNext',
 
-  LANDING_HEADER_BUTTON_2_TITLE: '作者博客',
-  LANDING_HEADER_BUTTON_2_URL: 'https://blog.tangly1024.com/',
+  //LANDING_HEADER_BUTTON_2_TITLE: '作者博客',
+  //LANDING_HEADER_BUTTON_2_URL: 'https://blog.tangly1024.com/',
 
   // 首页大图英雄板块
   LANDING_HERO_TITLE_1: 'NotionNext',
