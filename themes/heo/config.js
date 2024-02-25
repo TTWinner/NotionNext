50d8f5b4324a71edb0b02cebdd202289f33c55a7
TTWinner <118160230+TTWinner@users.noreply.github.com>
2024-02-25 14:45:23 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -17,14 +17,14 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '量化分析',
   HEO_HERO_TITLE_2: '超級趨勢圖',
-  HEO_HERO_TITLE_3: 'supertrend.robottrading.com.k',
+  HEO_HERO_TITLE_3: 'https://supertrend.robottrading.com.hk',
   HEO_HERO_TITLE_4: '今日小tips',
   HEO_HERO_TITLE_5: '市場將會大幅反彈',
   HEO_HERO_TITLE_LINK: 'https://www.facebook.com/RobotTrading.com.hk/posts/pfbid0jX4L9M6J2MER8RVNrYMEh2HZ4JSPHNthnzBi74dBgHKtNCMKEyKK3BNnds3MtNEkl',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_1: { title: '超級趨勢圖', url: 'https://supertrend.robottrading.com.hk' },
+  HEO_HERO_CATEGORY_2: { title: '今日小tips', url: '/tag/今日小tips' },
   HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
